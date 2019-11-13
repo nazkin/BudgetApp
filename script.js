8bc03c85daeddeff50aad10ee767c367ec9b4419
nazkin <nazar.kinash93@gmail.com>
2019-11-13 16:59:05 -0500

    Data Structure Created

--- script.js
+++ script.js
@@ -1,31 +1,44 @@
 
-//We will structure everything in modules
-//So encapsulate data accordingly for privacy reasons
-//We will have 3 modules initially 
-//1) UI MODULE
-        // Get Input Values
-        // Add new items to the UI 
-        // Update the UI
-//2) INTERNAL DATA HANDLING 
-        //Add the new Items into our data structure
-        // Calculate the budget 
-//3) CONTROLLER MODULE (link)
-        // Add Event Handler
 
 //FIRST MODULE:CONTROLLER**********************************************************
 var budgetControl = (function() {/////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+//Goal: Create a function constructor that will be used to instantiate all the input objects
+//Each input object will have 3 values--> 1) type 2) description 3) value 
 
+    var Income = function(id,description,value) { //Constructor for income inputs
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
 
+    var Expenses = function(id,description,value) { //Constructor for expense inputs
+        this.id = id;
+        this.description = description;
+        this.value = value;
+        };
 
-/////////////////////////////////////////////////////////////////////////////////////////
+//Our DATA STRUCTURE will include an object containing two identifiers 1)totals - (for total inc and exp) and  2)allEntries -(storing all inc and exp entries)   
+        var data = {
+            allEntries: {
+                inc: [],
+                exp: []
+            },
+
+            totals:{
+                inc: [],
+                exp: []
+            }
+        };
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
 })();/////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////
 //SECOND MODULE: UICONTROL************************************************************
 ////////////////////////////////////////////////////////////////////////////
 var uiControl = (function() {//////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 var everythingDOM = {
     type: '.add__type',
@@ -37,31 +50,48 @@ var everythingDOM = {
 
 //Public function used by central controller to read user input 
     return {
-        getInput: function() {
+        getInput: function() {         //read all 3 inputs from the page
+
             return {
                 inputType: document.querySelector(everythingDOM.type).value, // + for inc and - for exp
                 inputDescription: document.querySelector(everythingDOM.description).value, // text descriptor
                 inputValue: document.querySelector(everythingDOM.value).value //numerical value
             };
         },
-        getDOMvalues: function() {
+        getDOMvalues: function() {         //return all DOM values to the global scope such that they can be used everywhere needed
+
             return everythingDOM;
         }
     
 };
 
-/////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 })();////////////////////////////////////////////////////////////////////////////
 //THIRD MODULE: CENTRAL CONTROL ********************************************************
 var centralControl = (function (uiCtrl,bgtCtrl){//////////////////////////////////////////////
- ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+ //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   
-    var infoFromDOM = uiCtrl.getDOMvalues();
+    var allEventListeners = function() {
+
+        var infoFromDOM = uiCtrl.getDOMvalues();//read dom selectors from UI controller
+
+        document.querySelector(infoFromDOM.inputBtn).addEventListener('click',centralAddItem ); //Add items on click
+        document.addEventListener('keypress', function(event){//Add items on 'enter'
+
+            if(event.keyCode === 13){
+                centralAddItem();
+            }
+
+        });
+    
+    };
+
+    
 
      var centralAddItem = function() {
         //1) Read Input Data from the fields
         var completeInput = uiCtrl.getInput();
-        console.log(completeInput);
+        
         //2) Add Input Item to budgetControl module 
 
 
@@ -74,17 +104,17 @@ var centralControl = (function (uiCtrl,bgtCtrl){////////////////////////////////
         //5)Display this budget on the UI
         
 
-  }
+  };
     
-    //Selecting the input button and the enter key for entering user data and calculating it
- 
-    document.querySelector(infoFromDOM.inputBtn).addEventListener('click',centralAddItem ); //Add items on click
-    document.addEventListener('keypress', function(event){//Add items on 'enter'
-        if(event.keyCode === 13){
-            centralAddItem();
-        }
-    });
+  return {
+      init: function() {  //initialization funciton to help us start the program effectively
 
- ///////////////////////////////////////////////////////////////////////////////////////
+          console.log("Init function works");
+          allEventListeners();
+      }
+  };
+ /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 })(uiControl,budgetControl);/////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
+centralControl.init();
+
